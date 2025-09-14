38b1f507be74679070286377b60da9a2298cefc5
deBilla <dimuthu.billa@gmail.com>
2025-09-14 15:21:06 +0800

    fix

---
+++ src/app/categories/page.js
@@ -0,0 +1,8 @@
+import OfferBrowser from '../components/OfferBrowser';
+import data from '../api/data.json';
+
+// This is a Server Component. It runs on the server to fetch data.
+export default async function Categories() {
+  const allOffers = data;
+  return <OfferBrowser initialOffers={allOffers} />;
+}
