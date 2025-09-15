59b718c2d6cb6cbca2a97de68635df29e0d4ea4a
deBilla <dimuthu.billa@gmail.com>
2025-09-15 23:08:05 +0800

    fix

---
+++ src/app/bank/[id]/page.js
@@ -0,0 +1,8 @@
+import OfferBrowser from '../../components/OfferBrowser';
+import data from '../../api/data.json';
+
+// This is a Server Component. It runs on the server to fetch data.
+export default async function Banks() {
+  const allOffers = data;
+  return <OfferBrowser initialOffers={allOffers} />;
+}
